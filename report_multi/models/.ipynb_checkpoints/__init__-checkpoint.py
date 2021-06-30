5dcd53a0affb5eff5f258c07d9e5c981c58d3b79
Jhair Escobar <je@todoo.co>
2021-06-30 16:25:19 -0500

    [ADD] report_sabana: account move line report

--- report_multi/models/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,8 +0,0 @@
-
-from . import res_partner
-from . import res_partner_zone
-from . import sale_order
-from . import stock_picking
-from . import stock_move
-from . import res_partner_type
-from . import product
