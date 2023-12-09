ad8ac92758f6152166dba1b6e319ffc39fa1a82b
Coondev CO <79494742+odooco@users.noreply.github.com>
2023-12-09 09:14:37 -0500

    [ADD]custom_multy

---
+++ custom_multy/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Configuraciones Multy',
+    'version': '13.0',
+    'category': 'Sales',
+    'author': 'Coondev - Daniers D.',
+    'description': """Manage Configuration of Aceros & Aluminios.""",
+    'license': 'LGPL-3',
+    'depends': ['base','sale'],
+    'images': ['static/description/icon.png'],
+    'data': [
+       
+        'reports/sale_report.xml',
+       
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
